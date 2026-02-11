1ec9ddbed779f4be8361b78d8fa3fff5f333bb05
Thales_Dev <thalesmda@gmail.com>
2026-02-11 19:29:46 -0300

    meu projeto conta banco

---
+++ Conta Banco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+/**
+ * ContaTerminal - Aplicação de Terminal Bancário
+ * 
+ * Criado por @ThalesDev
+ * 
+ * Classe responsável por gerenciar a criação e configuração inicial de uma conta bancária
+ * através de uma interface gráfica (JOptionPane).
+ * 
+ * A aplicação solicita ao usuário as seguintes informações:
+ * - Número da conta
+ * - Número da agência
+ * - Nome do cliente
+ * - Saldo inicial da conta
+ * 
+ * Após coletar todas as informações, exibe uma mensagem de confirmação com os dados cadastrados.
+ * 
+ * @author ThalesDev
+ * @version 1.0
+ * @since 2025
+ */
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+         System.out.println("Hello, World!");
+
+
+    }
+}
